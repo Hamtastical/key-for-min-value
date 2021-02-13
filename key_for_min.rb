cf7abce2dc7b7ea409e6b1e4aac77c371f3559f2
meili <meiliham1@gmail.com>
2021-02-13 02:28:43 -0500

    commit tree 4b6aff48b9063e0f43499a7cc0e7ba40c09ded96

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    minimum = ""
+    minimum = name_hash.inject {|key,value| key.last > value.last ? value : key }
+        if name_hash == {}
+        minimum
+    else
+        minimum.first
+ end
 end
+
